dab8fbe325a85dcc7ab71d8c6b7641483a64b0bf
sarikamahboob <sarika.mahboob1234@gmail.com>
2024-05-19 22:21:24 +0600

    fix: production workflow fle fixed
    Signed-off-by: sarikamahboob <sarika.mahboob1234@gmail.com>

---
+++ release.config.js
@@ -0,0 +1,17 @@
+const config = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/git', {
+        assets: ['CHANGELOG.md', 'package.json', "dist/*.js", "dist/*.map"],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+      },
+    ],
+    "@semantic-release/github"
+  ]
+};
+
+module.exports = config;
