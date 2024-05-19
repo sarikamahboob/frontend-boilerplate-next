83ad25715269779a64c9e85954aae4d3ca3bd520
sarikamahboob <sarika.mahboob1234@gmail.com>
2024-05-20 00:23:26 +0600

    feat: text added

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <main>
       <h1>HOLA!! How are you ??</h1>
+      <h2>WELL 😑</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta iure iste tempora, rem ducimus molestias similique illum numquam dolorum optio perferendis voluptatum sapiente impedit cumque facere ratione maxime fuga ipsum.</p>
     </main>
   );
