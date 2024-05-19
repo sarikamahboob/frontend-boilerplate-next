867971749c7cf2d27441312a0e62f5b7eb2152e4
sarikamahboob <sarika.mahboob1234@gmail.com>
2024-05-20 00:18:08 +0600

    fix: commitlit config ile updated with rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,8 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+// commitlint.config.js
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 72],
+    'body-max-line-length': [2, 'always', 200], // Adjust this value as needed
+  },
+};
