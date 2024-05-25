fc7fc606b80a53378371fefde6632ab5cc83c602
sarika <sarika.mahboob1234@gmail.com>
2024-05-26 01:00:01 +0600

    some changes made for serving  in server

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,3 +1,4 @@
+// "use client"
 import { notFound } from 'next/navigation'
 import React from 'react'
 
@@ -12,3 +13,27 @@ const SingleBlogPage = ({params}:any) => {
 }
 
 export default SingleBlogPage
+
+
+export async function generateStaticParams() {
+  // Fetch blog post IDs or define them manually
+  const blogPosts = await fetch('https://jsonplaceholder.typicode.com/posts') // Replace with your API endpoint
+    .then(response => response.json());
+
+  // Map the fetched posts to the required format
+  const blogPostIds = blogPosts.map((post: any) => ({
+    id: post.id.toString(),
+  }));
+
+  // Manually include any required IDs that might be missing
+  const requiredIds = [{ id: '1' }, { id: '2' }];
+  const allIds = [...requiredIds, ...blogPostIds];
+
+  // Ensure there are no duplicates
+  const uniqueIds = Array.from(new Set(allIds.map(a => a.id))).map(id => ({
+    id,
+  }));
+
+  return uniqueIds;
+}
+
